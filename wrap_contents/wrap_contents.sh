f810d5393e1cd482b0ff2ad6c475b7958ef302f2
reuterShawn <reutershawn@LAPTOP-963ETN0S.localdomain>
2020-08-31 22:19:11 -0500

    Add functionality to wrap_contents.sh executable
    Added cat command to combine three files together resulting in a file named
    my_chart.html

--- wrap_contents/wrap_contents.sh
+++ wrap_contents/wrap_contents.sh
@@ -3,11 +3,15 @@
 # Script that takes three arguments and combines the three files together.
 # The last argument is the new name for the combination of the three files.
 
-# Naming the first three arguments that will be taken in
-unwrappedFile=$1
-contents=$2
-wrappedFile=$3
+# Assigning descriptive names to the first three arguments that will be taken in
+  unwrappedFile=$1
+  contents=$2
+  wrappedFile=$3
 
 
+# Assigning descriptive names to files
+  header="$contents"_header.html
+  footer="$contents"_footer.html
 
-
+# Combining three files to create a file called my_chart.html
+  cat "$header" "$unwrappedFile" "$footer" > "$wrappedFile"
