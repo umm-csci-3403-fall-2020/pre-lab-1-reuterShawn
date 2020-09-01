73d3dedee6c95a7a0c93d700145f7830b072c144
reuterShawn <reutershawn@LAPTOP-963ETN0S.localdomain>
2020-08-31 20:53:14 -0500

    Create wrap_contents.sh executable
    Created executable and assigned names to the first three arguments that would be
    passed in.
    names.

---
+++ wrap_contents/wrap_contents.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Script that takes three arguments and combines the three files together.
+# The last argument is the new name for the combination of the three files.
+
+# Naming the first three arguments that will be taken in
+unwrappedFile=$1
+contents=$2
+wrappedFile=$3
+
+
+
+
