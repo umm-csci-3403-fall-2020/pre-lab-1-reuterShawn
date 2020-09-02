b82ea5505eb63624e85c6d0b8761de58d08828da
reuterShawn <reutershawn@LAPTOP-963ETN0S.localdomain>
2020-09-02 09:42:52 -0500

    Add awk expression to match specific words
    Added awk expression using regex to match specific words with the result
    being stored in r1_output.txt

--- regex/regex.sh
+++ regex/regex.sh
@@ -4,5 +4,12 @@
 # This script uses tools like sed or awk using regex to extract the desired content.
 
 
-# Sed using regex and saves the result as r0_output.txt 
+# Sed using regex and saving the result as r0_output.txt 
   sed -E 's/\* ([a-zA-Z]+), ([a-zA-Z]+)/1. \1\n2. \2\n/' < r0_input.txt > r0_output.txt
+
+# awk using regex and saving the result as r1_output.txt  
+  awk 'match($0, /* I am (\w+). My favorite sandwich is (\w+)./, groups) {print "1. " groups[1] "\n" "2. " groups[2] "\n" }' < r1_input.txt > r1_output.txt
+
+ 
+
+
