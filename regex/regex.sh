95e72cf8404771e2fb9f39fc4500efef86ba8e8b
reuterShawn <reutershawn@LAPTOP-963ETN0S.localdomain>
2020-09-02 10:27:31 -0500

    Add sed expression for final r2_output.txt
    Added sed expression using regex to remove the unnecessary words and then
    be left with the resulting output.

--- regex/regex.sh
+++ regex/regex.sh
@@ -10,6 +10,7 @@
 # awk using regex and saving the result as r1_output.txt  
   awk 'match($0, /* I am (\w+). My favorite sandwich is (\w+)./, groups) {print "1. " groups[1] "\n" "2. " groups[2] "\n" }' < r1_input.txt > r1_output.txt
 
- 
+# Sed using regex and saving the result as r2_output.txt
+  sed -E 's/\* sandwich with ([a-zA-Z\.]+) ([a-zA-Z ]+)/1. \1\n2. \2\n/' < r2_input.txt >  r2_output.txt 
 
 
